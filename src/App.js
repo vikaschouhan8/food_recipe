4dab143319c79f6711a39dc78b1f16437fbd5e59
vikaschouhan8 <aboutvikaschouhan@gmai.com>
2019-08-31 18:45:15 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+    render() {
+        return (
+            <>
+                <h1>Hey food!!</h1>
+            </>
+        );
+    }
+}
+
+export default App;
