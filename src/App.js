8ce95b6d947c113d83fcf731bcfd9ec06d22d9ae
vikaschouhan8 <aboutvikaschouhan@gmai.com>
2019-09-01 01:14:59 +0530

    add search functionality with proper validation

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import Recipe from './Recipe';
 const App = () => {
     const APP_ID = 'dbca0ff5'
     const APP_KEY = 'f95679a0f4c17ccff2085f31d00980eb'
-    const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
 
     // const [counter, setCounter] = useState(0)
     const [recipes, setRecipes] = useState([])
-    
+    const [search, setSearch] = useState('')
+    const [query, setQuery] = useState('chicken')
+
+    const exampleReq = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+
     useEffect(()=>{
         getResponse()
-    }, [])
+    }, [query])
 
     const getResponse = async () => {
         const resonse = await fetch(exampleReq)
@@ -20,10 +23,23 @@ const App = () => {
         console.log(data.hits)
         setRecipes(data.hits)
     }
+
+    const updateSearch = (e) => {
+        setSearch(e.target.value)
+        // console.log(search)
+    }
+
+    const getSearch = e =>{
+        e.preventDefault()
+        setQuery(search)
+    }
+
     return(
         <div className="App">
-            <form className="search-bar">
-                <input className="search-bar" type="text"/>
+            <form onSubmit={getSearch} className="search-bar">
+                <input className="search-bar" type="text" 
+                    value={search} 
+                    onChange={updateSearch}/>
                 <button className="search-button" type="submit">Search</button>
             </form>
             {/* <h1 onClick={()=>setCounter(counter+1)}>{counter}</h1> */}
@@ -32,6 +48,7 @@ const App = () => {
                     <Recipe key={i} title={recipe.recipe.label}
                         calories={recipe.recipe.calories}
                         image={recipe.recipe.image}
+                        ingredients={recipe.recipe.ingredients}
                     />
                 ))
             }
--- src/Recipe.js
+++ src/Recipe.js
@@ -5,6 +5,15 @@ const Recipe = (props) => {
         <div>
             <h1>Title: {props.title}</h1>
             <p>Calories: {props.calories}</p>
+            <ol>
+                {
+                    props.ingredients.map((ingredient,i)=>{
+                        return (<li key={i}>
+                            {ingredient.text}
+                        </li>)
+                    })
+                }
+            </ol>
             <img src={props.image} alt={props.title} />
         </div>
     )
