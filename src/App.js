d2c93cadc2f850b4c900daa9841c162fb12df3ba
vikaschouhan8 <aboutvikaschouhan@gmai.com>
2019-08-31 23:53:49 +0530

    useEffect component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React, { Component } from 'react';
+import React, {useEffect, useState} from 'react';
+import './App.css'
 
+const App = () => {
+    const APP_ID = 'dbca0ff5'
+    const APP_KEY = 'f95679a0f4c17ccff2085f31d00980eb'
+    const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
 
-class App extends Component {
-    render() {
-        return (
-            <>
-                <h1>Hey food!!</h1>
-            </>
-        );
-    }
+    const [counter, setCounter] = useState(0)
+
+    useEffect(()=>{
+        console.log("useEffect has been called!!")
+    }, [])
+    return(
+        <div className="App">
+            <form className="search-bar">
+                <input className="search-bar" type="text"/>
+                <button className="search-button" type="submit">Search</button>
+            </form>
+            {/* <h1 onClick={()=>setCounter(counter+1)}>{counter}</h1> */}
+        </div>
+    )
 }
 
-export default App;
+export default App
